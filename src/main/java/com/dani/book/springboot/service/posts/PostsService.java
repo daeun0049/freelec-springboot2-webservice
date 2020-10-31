e0eb0cb61df38fae80e23fc30a1b910d52653760
daeun0049 <daeun0049@gmail.com>
2020-10-31 15:21:34 +0900

    chapter4. 등록, 조회

--- src/main/java/com/dani/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/dani/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,17 @@ package com.dani.book.springboot.service.posts;
 
 import com.dani.book.springboot.domain.posts.Posts;
 import com.dani.book.springboot.domain.posts.PostsRepository;
+import com.dani.book.springboot.web.dto.PostsListResponseDto;
 import com.dani.book.springboot.web.dto.PostsResponseDto;
 import com.dani.book.springboot.web.dto.PostsSaveRequestsDto;
 import com.dani.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +40,12 @@ public class PostsService {
                 IllegalArgumentException("해당 게시글이 없습니다. id="+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
